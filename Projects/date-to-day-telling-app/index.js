de3a37fe0ad15a24aa3f79c313e05b9f7487ac75
Wamique Abro <wamik.abro212@gmail.com>
2024-08-12 15:28:01 +0500

    connect all the classes and ids to js

---
+++ Projects/date-to-day-telling-app/index.js
@@ -0,0 +1,8 @@
+const dayInput = document.querySelector("#day-input");
+const monthInput = document.querySelector("#month-input");
+const yearInput = document.querySelector("#year-input");
+const submitButton = document.querySelector("#submit-button");
+
+const result = document.querySelector(".result");
+const dateContainer = document.querySelector("#date-container");
+const dayContainer = document.querySelector("#day-container");

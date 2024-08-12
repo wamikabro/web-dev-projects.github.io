6376748615533dae153d3fd8c1abfbc1122d96db
Wamique Abro <wamik.abro212@gmail.com>
2024-08-12 16:15:31 +0500

    velidate inputs and selectors before submit button clicked

--- Projects/date-to-day-telling-app/index.js
+++ Projects/date-to-day-telling-app/index.js
@@ -6,3 +6,33 @@ const submitButton = document.querySelector("#submit-button");
 const result = document.querySelector(".result");
 const dateContainer = document.querySelector("#date-container");
 const dayContainer = document.querySelector("#day-container");
+
+dayInput.addEventListener("input", () => {
+  minAndMaxValueValidator(dayInput, "minAndmax");
+});
+
+yearInput.addEventListener("input", () => {
+  minAndMaxValueValidator(yearInput, "min");
+});
+
+// Validate inputs before submission
+function minAndMaxValueValidator(selector, checkValidationOf) {
+  if (checkValidationOf == "min") {
+    if (+selector.value < selector.min) {
+      // using + operator to automatically parse a value to Integer,
+      // and since one value is Integer, the second will be implicitly made Integer
+      selector.value = selector.min;
+    }
+  } else if (checkValidationOf == "max") {
+    if (+selector.value > selector.max) {
+      selector.value = selector.max;
+    }
+  } else if (checkValidationOf == "minAndmax") {
+    if (+selector.value < selector.min) {
+      selector.value = selector.min;
+    }
+    if (+selector.value > selector.max) {
+      selector.value = selector.max;
+    }
+  }
+}

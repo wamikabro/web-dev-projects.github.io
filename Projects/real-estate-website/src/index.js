ed871c4c9c4f1d213c7619304cdba40bb2a49185
Wamique Abro <wamik.abro212@gmail.com>
2024-09-10 20:28:12 +0500

    toggle hamburger icon

--- Projects/real-estate-website/src/index.js
+++ Projects/real-estate-website/src/index.js
@@ -1,5 +1,5 @@
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const navItemsSection = document.querySelector(".nav-items-section");
 hamburgerIcon.addEventListener("click", () => {
-  navItemsSection.classList.add("nav-items-section-show");
+  navItemsSection.classList.toggle("nav-items-section-toggle");
 });

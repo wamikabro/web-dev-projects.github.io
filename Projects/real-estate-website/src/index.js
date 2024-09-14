0ebcca44f0ad3773fceb3255bd076ab71a86d99d
Wamique Abro <wamik.abro212@gmail.com>
2024-09-14 15:59:27 +0500

    toggle between hamburger and close icon

--- Projects/real-estate-website/src/index.js
+++ Projects/real-estate-website/src/index.js
@@ -1,5 +1,15 @@
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const navItemsSection = document.querySelector(".nav-items-section");
+const closeIcon = document.querySelector(".close-icon");
+
 hamburgerIcon.addEventListener("click", () => {
-  navItemsSection.classList.toggle("nav-items-section-toggle");
+  hamburgerIcon.classList.add("hamburger-icon-hide");
+  navItemsSection.classList.add("nav-items-section-toggle");
+  closeIcon.classList.add("close-icon-show");
+});
+
+closeIcon.addEventListener("click", () => {
+  hamburgerIcon.classList.remove("hamburger-icon-hide");
+  navItemsSection.classList.remove("nav-items-section-toggle");
+  closeIcon.classList.remove("close-icon-show");
 });

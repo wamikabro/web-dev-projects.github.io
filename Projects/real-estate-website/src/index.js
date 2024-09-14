bce7ca557ca63eda33f0979e5c2606435a3072c7
Wamique Abro <wamik.abro212@gmail.com>
2024-09-14 16:04:47 +0500

    stop scrolling if responsive menu is open

--- Projects/real-estate-website/src/index.js
+++ Projects/real-estate-website/src/index.js
@@ -6,10 +6,12 @@ hamburgerIcon.addEventListener("click", () => {
   hamburgerIcon.classList.add("hamburger-icon-hide");
   navItemsSection.classList.add("nav-items-section-toggle");
   closeIcon.classList.add("close-icon-show");
+  document.body.classList.add("no-scroll");
 });
 
 closeIcon.addEventListener("click", () => {
   hamburgerIcon.classList.remove("hamburger-icon-hide");
   navItemsSection.classList.remove("nav-items-section-toggle");
   closeIcon.classList.remove("close-icon-show");
+  document.body.classList.remove("no-scroll");
 });

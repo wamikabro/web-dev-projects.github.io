fa49d0f8d2b1e6d7a448c9f6663a363925030e58
Wamique Abro <wamik.abro212@gmail.com>
2024-09-20 12:42:18 +0500

    make indicators clickable

--- Projects/crousel-testimonial-slider/script.js
+++ Projects/crousel-testimonial-slider/script.js
@@ -4,6 +4,13 @@ const rightArrow = document.querySelector(".right");
 
 let sectionIndex = 0;
 
+document.querySelectorAll(".controls ul li").forEach((indicator, index) => {
+  indicator.addEventListener("click", () => {
+    sectionIndex = index;
+    slider.style.transform = "translateX(" + sectionIndex * -25 + "%)";
+  });
+});
+
 rightArrow.addEventListener("click", () => {
   sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
   slider.style.transform = "translateX(" + sectionIndex * -25 + "%)";

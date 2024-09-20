a967cceccbfaa8296cd3593d3b48f3a80878e0dc
Wamique Abro <wamik.abro212@gmail.com>
2024-09-20 13:08:54 +0500

    use single function instead

--- Projects/crousel-testimonial-slider/script.js
+++ Projects/crousel-testimonial-slider/script.js
@@ -5,12 +5,16 @@ const indicatorParent = document.querySelector(".controls ul");
 
 let sectionIndex = 0;
 
+function setIndex(sectionIndex) {
+  slider.style.transform = "translateX(" + sectionIndex * -25 + "%)";
+  /* remove other selected indicators */
+  document.querySelector(".controls .selected").classList.remove("selected");
+}
+
 document.querySelectorAll(".controls ul li").forEach((indicator, index) => {
   indicator.addEventListener("click", () => {
     sectionIndex = index;
-    slider.style.transform = "translateX(" + sectionIndex * -25 + "%)";
-    /* remove other selected indicators */
-    document.querySelector(".controls .selected").classList.remove("selected");
+    setIndex(sectionIndex);
     /* impliment new selected indicator */
     indicator.classList.add("selected");
   });
@@ -18,16 +22,12 @@ document.querySelectorAll(".controls ul li").forEach((indicator, index) => {
 
 rightArrow.addEventListener("click", () => {
   sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
-  slider.style.transform = "translateX(" + sectionIndex * -25 + "%)";
-  /* remove other selected indicators */
-  document.querySelector(".controls .selected").classList.remove("selected");
+  setIndex(sectionIndex);
   indicatorParent.children[sectionIndex].classList.add("selected");
 });
 
 leftArrow.addEventListener("click", () => {
   sectionIndex = sectionIndex > 0 ? sectionIndex - 1 : 0;
-  slider.style.transform = "translateX(" + sectionIndex * -25 + "%)";
-  /* remove other selected indicators */
-  document.querySelector(".controls .selected").classList.remove("selected");
+  setIndex(sectionIndex);
   indicatorParent.children[sectionIndex].classList.add("selected");
 });
